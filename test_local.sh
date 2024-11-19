343a511b043e5d5bd0e53d818f836780d8c1614c
Martin O'Leary <martin.oleary@finbourne.com>
2024-11-19 09:35:54 +0000

    CAN-16: project: drop testing script

--- test_local.sh
+++ test_local.sh
@@ -2,13 +2,13 @@
 
 set -eEuo pipefail
 
-export TEST_API_MODULE=api.instrument_api
-export TEST_API=InstrumentApi
-export TEST_METHOD='get_open_figi_parameter_option(finbourne_horizon.models.OpenFigiParameterOptionName.CURRENCY,'
+export TEST_API_MODULE=api.apps_api
+export TEST_API=AppsApi
+export TEST_METHOD='print('
 
-export PACKAGE_NAME=candela_sdk
+export PACKAGE_NAME=candela
 export PROJECT_NAME=candela-sdk
-export APPLICATION_NAME=candela
+export APPLICATION_NAME=candelacd 
 export META_REQUEST_ID_HEADER_KEY=candela-meta-requestid
 export REPO_URL="https://pypi.org"
 export GIT_USER_ID=finbourne
